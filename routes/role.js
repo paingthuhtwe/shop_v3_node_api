bdae0817bc3d91180a64bc76f2e07be6a544c3ed
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-10-11 14:27:16 +0630

    add token for all needed routes

--- routes/role.js
+++ routes/role.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
 const controller = require('../controllers/role');
 const { RoleSchema, AllSchema } = require('../utils/schema')
-const { validateBody, validateParams } = require('../utils/validator')
+const { validateBody, validateParams, validateToken } = require('../utils/validator')
 
-router.get('/', controller.all)
-router.post('/', validateBody(RoleSchema.add), controller.add)
-router.post('/add/permit', validateBody(RoleSchema.addPermit), controller.roleAddPermit)
-router.delete('/remove/permit', validateBody(RoleSchema.addPermit), controller.roleRemovePermit)
+router.get('/', validateToken(), controller.all)
+router.post('/', validateToken(), validateBody(RoleSchema.add), controller.add)
+router.post('/add/permit', validateToken(), validateBody(RoleSchema.addPermit), controller.roleAddPermit)
+router.delete('/remove/permit', validateToken(), validateBody(RoleSchema.addPermit), controller.roleRemovePermit)
 
 router.route('/:id')
-    .get(validateParams(AllSchema.id, 'id'), controller.get)
-    .patch(validateBody(RoleSchema.add), validateParams(AllSchema.id, 'id'), controller.patch)
-    .delete(validateParams(AllSchema.id, 'id'), controller.drop)
+    .get(validateToken(), validateParams(AllSchema.id, 'id'), controller.get)
+    .patch(validateToken(), validateBody(RoleSchema.add), validateParams(AllSchema.id, 'id'), controller.patch)
+    .delete(validateToken(), validateParams(AllSchema.id, 'id'), controller.drop)
 
 module.exports = router;
--- utils/validator.js
+++ utils/validator.js
@@ -31,21 +31,32 @@ module.exports = {
   validateToken: () => {
     return async (req, res, next) => {
       try {
-        const token = req.headers.authorization.split(" ")[1];
-        const decode = jwt.verify(token, process.env.SECRET_KEY);
-        if (decode) {
-          const redisUserData = await Helper.redisGet(decode._id);
-          if (redisUserData) {
-            req.user = redisUserData;
-            next();
+        const token = req.headers.authorization;
+        if (token && token.startsWith('Bearer ')) { 
+          const realToken = token.split(' ')[1];
+          const decode = jwt.verify(realToken, process.env.SECRET_KEY);
+          if (decode) {
+            const redisUserData = await Helper.redisGet(decode._id);
+            if (redisUserData) {
+              req.user = redisUserData;
+              next();
+            } else {
+              const error = new Error('Token authorization failed');
+              error.status = 401;
+              next(error);
+            }
           } else {
-            const error = new Error("Token authorization error!");
+            const error = new Error('Invalid token');
             error.status = 401;
             next(error);
           }
+        } else {
+          const error = new Error('Invalid token format');
+          error.status = 401;
+          next(error);
         }
       } catch (err) {
-        const error = new Error("Token authorization error!");
+        const error = new Error(err.message);
         error.status = 401;
         next(error);
       }
