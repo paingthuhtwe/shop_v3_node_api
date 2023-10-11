6f009db94c71c553a4764e6bcf013d29b26b576d
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-10-11 12:46:50 +0630

    verify with jwt token users list

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const controller = require('../controllers/user');
-const { validateBody } = require('../utils/validator');
+const { validateBody, validateToken } = require('../utils/validator');
 const { UserSchema } = require('../utils/schema');
 
 router.post('/register', validateBody(UserSchema.register), controller.register);
 router.post('/login', validateBody(UserSchema.login), controller.login);
-router.get('/users', controller.all);
+router.get('/users', validateToken(), controller.all);
 
 module.exports = router;
--- utils/validator.js
+++ utils/validator.js
@@ -1,9 +1,14 @@
+const jwt = require("jsonwebtoken");
+const Helper = require("./helper");
+
 module.exports = {
   validateBody: (schema) => {
     return (req, res, next) => {
       let result = schema.validate(req.body);
       if (result.error) {
-        next(new Error(result.error.details[0].message));
+        const error = new Error(result.error.details[0].message);
+        error.status = 400;
+        next(error);
       } else {
         next();
       }
@@ -12,12 +17,37 @@ module.exports = {
   validateParams: (schema, name) => {
     return (req, res, next) => {
       let obj = {};
-      obj[`${name}`] = req.params[`${name}`]
-      let result = schema.validate(obj)
+      obj[`${name}`] = req.params[`${name}`];
+      let result = schema.validate(obj);
       if (result.error) {
-        next(new Error(result.error.details[0].message));
+        const error = new Error(result.error.details[0].message);
+        error.status = 400;
+        next(error);
       } else {
         next();
+      }
+    };
+  },
+  validateToken: () => {
+    return async (req, res, next) => {
+      try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decode = jwt.verify(token, process.env.SECRET_KEY);
+        if (decode) {
+          const redisUserData = await Helper.redisGet(decode._id);
+          if (redisUserData) {
+            req.user = redisUserData;
+            next();
+          } else {
+            const error = new Error("Token authorization error!");
+            error.status = 401;
+            next(error);
+          }
+        }
+      } catch (err) {
+        const error = new Error("Token authorization error!");
+        error.status = 401;
+        next(error);
       }
     };
   },
