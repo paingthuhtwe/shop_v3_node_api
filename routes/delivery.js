ddc9cbdd7a8849260c1fbd828e1415dd8e1df6d5
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-11-01 14:27:13 +0630

    add role validate to delivery

--- routes/delivery.js
+++ routes/delivery.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const controller = require('../controllers/delivery');
 const { DeliverySchema, AllSchema } = require('../utils/schema');
-const { validateBody, validateFile, validateParams, validateToken } = require('../utils/validator');
+const { validateBody, validateFile, validateParams, validateToken, validateRole } = require('../utils/validator');
 
 router.get('/', controller.all);
-router.post('/', validateToken(), validateFile(AllSchema.image, 'image'), validateBody(DeliverySchema.add), controller.add);
+router.post('/', validateToken(), validateRole(['Owner', 'Manager', 'Supervisor']), validateFile(AllSchema.image, 'image'), validateBody(DeliverySchema.add), controller.add);
 router.route('/:id')
     .get(validateParams(AllSchema.id, 'id'), controller.get)
-    .patch(validateToken(), validateParams(AllSchema.id, 'id'), controller.patch)
-    .delete(validateToken(), validateParams(AllSchema.id, 'id'), controller.drop)
+    .patch(validateToken(), validateRole(['Owner', 'Manager', 'Supervisor']), validateParams(AllSchema.id, 'id'), controller.patch)
+    .delete(validateToken(), validateRole(['Owner', 'Manager', 'Supervisor']), validateParams(AllSchema.id, 'id'), controller.drop)
 
 module.exports = router;
